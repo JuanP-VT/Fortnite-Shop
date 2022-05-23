9dbbd08441788cef78be05fcc97310bde77d81c7
JuanP-VT <cv.juanp@gmail.com>
2022-05-23 11:59:48 -0600

    test refactor

--- src/Tests/TopNav.test.tsx
+++ src/Tests/TopNav.test.tsx
@@ -1,21 +1,26 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import TopNav from "../Components/TopNav";
+import { BrowserRouter } from "react-router-dom";
 
 describe("Renders Text", () => {
+  beforeEach(() => {
+    render(
+      <BrowserRouter>
+        <TopNav />
+      </BrowserRouter>
+    );
+  });
   test("Renders Home", () => {
-    render(<TopNav />);
     const linkElement = screen.getByText(/home/i);
     expect(linkElement).toBeInTheDocument();
   });
 
   test("Renders Shop", () => {
-    render(<TopNav />);
     const linkElement = screen.getByText(/shop/i);
     expect(linkElement).toBeInTheDocument();
   });
   test("Renders Cart", () => {
-    render(<TopNav />);
     const linkElement = screen.getByText(/cart/i);
     expect(linkElement).toBeInTheDocument();
   });
