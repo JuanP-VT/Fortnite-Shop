6a18cd72fa742dad2d12862807217c279d04ce55
JuanP-VT <cv.juanp@gmail.com>
2022-05-23 12:01:13 -0600

    Added links to top nav

--- src/Components/TopNav.tsx
+++ src/Components/TopNav.tsx
@@ -1,15 +1,13 @@
-import React, { Component } from "react";
-
-interface Props {}
-
-interface State {}
-
-class TopNav extends Component<Props, State> {
-  state = {};
-
-  render() {
-    return <div>TopNav</div>;
-  }
+import React from "react";
+import { Link } from "react-router-dom";
+function TopNav() {
+  return (
+    <div id="topNav">
+      <Link to="/">Home</Link>
+      <Link to="shop">Shop</Link>
+      <Link to="cart">Cart</Link>
+    </div>
+  );
 }
 
 export default TopNav;
